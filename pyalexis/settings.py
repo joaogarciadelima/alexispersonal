1c6f5d4d6f249ad069cb92c9cdf7484910beda0c
tamaracl <tamara.decastro@live.com>
2020-10-18 22:29:01 -0300

    DEcouple

--- pyalexis/settings.py
+++ pyalexis/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hf3su1$t6nm#dq==o$y66-v^+)nw3ho7pwd$k-ggl06ivd)^ef'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
