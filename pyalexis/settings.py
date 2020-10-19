cec4175d0c28adce9efb3f83e655f978bd61cbe5
tamaracl <tamara.decastro@live.com>
2020-10-18 22:13:27 -0300

    DEcouple

--- pyalexis/settings.py
+++ pyalexis/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hf3su1$t6nm#dq==o$y66-v^+)nw3ho7pwd$k-ggl06ivd)^ef'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
