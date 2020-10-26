d5816f431a5715c340963ae5d5df01dde6feb349
tamaracl <tamara.decastro@live.com>
2020-10-25 23:52:24 -0300

    migrate heroku postgre

--- pyalexis/settings.py
+++ pyalexis/settings.py
@@ -19,7 +19,6 @@ from decouple import config, Csv
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -77,6 +76,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pyalexis.wsgi.application'
+
+# Configuração django toolbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
--- pyalexis/urls.py
+++ pyalexis/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
@@ -22,11 +23,7 @@ urlpatterns = [
     path('', include('pyalexis.base.urls')),
 ]
 
-
 if settings.DEBUG:
-    import debug_toolbar
-    from django.conf.urls.static import static
     urlpatterns.append(
-        path('__debug__/', include(debug_toolbar.urls))
+        path('__debug__/', include(debug_toolbar.urls)),
     )
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
